30043d68035c450cc8447311079eb8d6632147b2
ValiaL <wahia83@gmail.com>
2015-04-13 13:35:19 +0300

    Add map-view.js

---
+++ js/views/map-view.js
@@ -0,0 +1,18 @@
+/*global $ */
+/*jshint unused:false */
+var app = app || {};
+
+(function ($){
+	'use strict';
+
+       function initialize() {
+        var mapOptions = {
+          center: { lat: -34.397, lng: 150.644},
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+  
+})(jQuery);
