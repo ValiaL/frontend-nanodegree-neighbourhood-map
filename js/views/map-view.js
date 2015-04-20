7ee7f7ecc0a281ef02600fe70b21c144d7bd76c0
ValiaL <wahia83@gmail.com>
2015-04-20 12:34:00 +0300

    Add marker with hardcoded address

--- js/views/map-view.js
+++ js/views/map-view.js
@@ -3,12 +3,26 @@
 var app = app || {};
 
        function initialize() {
+
+
         var mapOptions = {
+
           center: { lat: 37.975892, lng: 23.733990},
           zoom: 8
+       
         };
+
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
+
+
+        var marker = new google.maps.Marker({
+
+             position: {lat: 37.995207, lng: 23.763363},
+             map: map,
+             title: 'Hello World!'
+            });
+
       }
       google.maps.event.addDomListener(window, 'load', initialize);
   
