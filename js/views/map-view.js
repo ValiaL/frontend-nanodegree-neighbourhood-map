e6e7666be094aa23bb7a9f3a2b15404a5544fdb5
ValiaL <wahia83@gmail.com>
2015-04-20 14:23:22 +0300

    Connect view model to model

--- js/views/map-view.js
+++ js/views/map-view.js
@@ -2,7 +2,30 @@
 /*jshint unused:false */
 var app = app || {};
 
-       function initialize() {
+var model = {
+       name : 'Yellow Cafe',
+       mapOptions : {
+            center: { lat: 37.995207, lng: 23.763363},
+            zoom: 16
+            } 
+    }                  
+
+
+ var ViewModel = function(){
+          /**
+           * self is used by child methods
+           *   to refer to the root object easily
+          */
+          var self=this;
+
+          /**
+           * An array of google.maps.Marker() objects
+           *   used by self.setNearByMarkers() to set
+           *   markers on the map.
+           */
+          this.dailyMarkers = ko.observableArray([]);
+
+        function initialize() {
 
 
         var mapOptions = {
@@ -18,11 +41,16 @@ var app = app || {};
 
         var marker = new google.maps.Marker({
 
-             position: {lat: 37.995207, lng: 23.763363},
-             map: map,
-             title: 'Hello World!'
+             position: {model.mapOptions.center.lat,model.mapOptions.center.lng},
+             map: self.map,
+             title: model.mapOptions.name
             });
 
       }
+
       google.maps.event.addDomListener(window, 'load', initialize);
+
+      }
+
+      
   
